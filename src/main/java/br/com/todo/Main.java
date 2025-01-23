3a2a26e2e4d2b169c3e571afa43b7f7a8d76c867
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-01-23 19:44:20 -0300

    feat: logica do arquivo main

--- src/main/java/br/com/todo/Main.java
+++ src/main/java/br/com/todo/Main.java
@@ -1,9 +1,23 @@
 package br.com.todo;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import br.com.todo.db.SQLiteConnection;
+import br.com.todo.db.TaskDAO;
+import br.com.todo.db.UserDAO;
+import br.com.todo.view.UserView;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        //TODO
+        SQLiteConnection.createTables();
+
+        Connection conn = SQLiteConnection.connect();
+        UserDAO usuarioProxy = new UserDAO(conn);
+        TaskDAO taskProxy = new TaskDAO(conn);
+
+        UserView usuarioView = new UserView(conn);
+        usuarioView.menu();
+
+        SQLiteConnection.close();
     }
 }
